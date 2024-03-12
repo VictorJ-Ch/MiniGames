3deb7bfa9bf570c133ba3ea1c9b4ae37e22dcec7
ChristopherOrtiz69 <112641049+ChristopherOrtiz69@users.noreply.github.com>
2024-03-12 10:28:59 -0600

    UI 1

---
+++ Assets/Scripts/HW/SistemaVida.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SistemaVida : MonoBehaviour
+{
+    public float maxVida = 100.0f;
+    public float actualVida;
+
+    public bool inmortal = false;
+    public float tiempoInmortal = 1.0f;
+
+    public GameObject explosionPrefab;
+
+    private void Start()
+    {
+        actualVida = maxVida;
+    }
+
+    private void Update()
+    {
+        if (actualVida > maxVida)
+            actualVida = maxVida;
+
+        if (actualVida <= 0)
+            Muerte();
+
+        // UI
+        
+    }
+
+    public void QuitarVida(float daño)
+    {
+        if (inmortal) return;
+
+        actualVida -= daño;
+        StartCoroutine(TiempoInmortal());
+    }
+
+    public void DarVida(float vida)
+    {
+        actualVida += vida;
+    }
+
+    public void Muerte()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator TiempoInmortal()
+    {
+        inmortal = true;
+        yield return new WaitForSeconds(tiempoInmortal);
+        inmortal = false;
+    }
+}
+
