ac89c8032f80ba3b37ace5ba0dc24a2e608f860e
Tobias Gtz <perepump@gmail.com>
2024-03-21 12:24:48 -0600

    Game3

--- Assets/Scripts/roll/RollController.cs
+++ Assets/Scripts/roll/RollController.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class RollController : MonoBehaviour
 {
     public float angularSpeed;
-    private float xAngle;
-
+    float xAngle;
     void Update()
     {
         float vInput = Input.GetAxis("Horizontal");
-        xAngle += angularSpeed * vInput * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(xAngle, 0, 0);   
+        xAngle += angularSpeed * vInput* Time.deltaTime;
+        transform.rotation = Quaternion.Euler(xAngle, 0 , 0);
     }
 }
