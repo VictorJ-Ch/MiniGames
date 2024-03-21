d8dd9503f78fcbe440dc2658aeb4741265b8698d
Tobias Gtz <perepump@gmail.com>
2024-03-21 12:52:36 -0600

    4 Players mode READY!!!

---
+++ Assets/Scripts/Rotisserie Rampage/Players/RollRunnerController1.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+
+public class RollRunnerController1 : MonoBehaviour
+{
+    public float angularSpeed, radius;
+    float xPosition, angle;
+    bool isPlaying = true;
+    void Start()
+    {
+        xPosition = transform.localPosition.x;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    void Update()
+    {
+        if(isPlaying)
+        {
+            PlayerMovement();
+        }
+    }
+    void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical-P2");
+        angle += angularSpeed * verticalInput * Time.deltaTime;
+        transform.localPosition = AngularPosition();
+        Vector3 up = AngularPosition();
+        up.x = 0;
+        Vector3 forward = Vector3.zero;
+        if(verticalInput >= 0) {forward = Vector3.Cross(Vector3.right, up);}
+        else{forward = -Vector3.Cross(Vector3.right, up);}
+        transform.localRotation = Quaternion.LookRotation(forward, up);
+    }
+    Vector3 AngularPosition()
+    {
+        float y = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float z = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        return new Vector3(xPosition, y, z);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("limit"))
+        {
+            Debug.Log("Outside");
+            Vector3 radialoPosition = AngularPosition();
+            radialoPosition.x = 0;
+            Vector3 slipDirection = Vector3.zero;
+            if(transform.position.z <= 0){slipDirection = Vector3.Cross(radialoPosition, Vector3.right).normalized;}
+            else{slipDirection = Vector3.Cross(radialoPosition, -Vector3.right).normalized;}
+            transform.parent =  null;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(5 * slipDirection, ForceMode. Impulse);
+            isPlaying = false;
+        }
+    }
+}
