cb1b0ab0a1635489982350a47e889570d2db7bb1
Tobias Gtz <perepump@gmail.com>
2024-04-09 10:04:04 -0600

    SoapSurfers V1

---
+++ Assets/Scripts/SoapSurfers/SliderPlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class SliderPlayer : MonoBehaviour
+{
+    public string horizontalInputName, verticalInputName;
+    public float forceMagnitude, torqueMagnitude;
+    public LayerMask bowlLayer;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovementControl();
+        Rotationrectification();
+    }
+
+    void MovementControl()
+    {
+        float dt = Time.deltaTime;
+        float hInput = Input.GetAxis(horizontalInputName);
+        float vInput = Input.GetAxis(verticalInputName);
+
+        Vector3 torque = torqueMagnitude * transform.up * hInput * dt;
+        Vector3 force = forceMagnitude * transform.forward * vInput * dt;
+        rb.AddTorque(torque, ForceMode.Force);
+        rb.AddForce(force, ForceMode.Force);
+    }
+
+    void Rotationrectification()
+    {
+        RaycastHit hit; 
+        if(Physics.Raycast(transform.position, -transform.up, out hit, 10, bowlLayer))
+        {
+            Vector3 newUp = hit.normal;
+            Vector3 newForward = transform.forward - Vector3.Dot(transform.forward, newUp) * newUp;
+            transform.rotation = Quaternion.LookRotation(newForward, newUp);
+        }
+    }
+
+    void MovementControl2()
+    {
+        float dt = Time.deltaTime;
+        float hInput = Input.GetAxis(horizontalInputName);
+        float vInput = Input.GetAxis(verticalInputName);
+
+        Vector3 InputDir = new Vector3(hInput, 0, vInput).normalized;
+        Vector3 newDir =  InputDir - Vector3.Dot(InputDir, transform.up) * transform.up;
+        Vector3 force = forceMagnitude * newDir * dt;
+        rb.AddForce(force, ForceMode.Force);
+    }
+}
