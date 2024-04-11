8997cad12bb70ba7d949a103b7723f263925e57e
Tobias Gtz <perepump@gmail.com>
2024-04-11 12:28:17 -0600

    Error Corrections
    :)

--- Assets/Scripts/SoapSurfers/BowlLimit.cs
+++ Assets/Scripts/SoapSurfers/BowlLimit.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BowlLimit : MonoBehaviour
 {
     Rigidbody rb;
-    void Update()
+    void Start()
     {
-       
+       rb = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
