84413459ab73d0a5bac2c9c555f779e595f9cace
JonathanZ <jonathan4zacos@gmail.com>
2024-05-07 09:47:27 -0600

    CODIGO

--- Assets/Scripts/cuerda/RopeMovement.cs
+++ Assets/Scripts/cuerda/RopeMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class RopeMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float swing;
+    public Transform fireBalls;
+    private BezierCurve _bezierCurve;
+    private float time;
     void Start()
     {
-        
+        _bezierCurve = GetComponent<BezierCurve>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        ControlPointsMovement();
+        FireBallsMovement();
+    }
+
+    void FireBallsMovement()
+    {
+        for (int i = 0; 1 < fireBalls.childCount; i++)
+        {
+            float si = (float)i/(fireBalls.childCount-1f);
+            fireBalls.GetChild(i).position = _bezierCurve.Bezier(si);
+        }
+    }
+
+    private void ControlPointsMovement()
+    {
+        float z1 = _bezierCurve.P[1].position.z;
+        float z2 = _bezierCurve.P[2].position.z;
+        _bezierCurve.P[1].position = CirclePath(5f, z1);
+        _bezierCurve.P[2].position = CirclePath(5f, z2);
+        time += Time.deltaTime;
+    }
+
+    Vector3 CirclePath(float radius, float ZCoordinate)
+    {
+        float xCoordinate = radius * Mathf.Sin(swing * time);
+        float yCoordinate = radius * Mathf.Cos(swing * time);
+        return new Vector3(xCoordinate, yCoordinate, ZCoordinate);
     }
 }
