16991be3e1bf1b827c927f047d4fecf9a03a0de8
Samgtzs <jonathan4zacos@gmail.com>
2024-05-09 12:12:13 -0600

    code PogoPlayer
    Co-Authored-By: Samgtzs <125500253+SamGtzs@users.noreply.github.com>

---
+++ Assets/Scripts/PogoAgogo/PogoPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PogoPlayer : MonoBehaviour
+{
+    public float forceImpulse, jumpImpulse;
+    public string hInputName, vInputName;
+    public float gravity;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Physics.gravity = gravity * Vector3.up;
+        float hInput = Input.GetAxis(hInputName);
+        float vInput = Input.GetAxis(vInputName);
+        Vector3 direction = new Vector3(hInput, 0, vInput).normalized;
+        rb.AddForce(forceImpulse * direction * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Base"))
+        {
+            rb.AddForce(forceImpulse * transform.up, ForceMode.Impulse);
+        }
+    }
+
+}
+
+
