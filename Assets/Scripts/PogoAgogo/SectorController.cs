57f20d1f553d62ca7c6b69116642b694dcb63ebb
Samgtzs <jonathan4zacos@gmail.com>
2024-05-09 12:28:33 -0600

    Code SectorController
    Co-Authored-By: Samgtzs <125500253+SamGtzs@users.noreply.github.com>

---
+++ Assets/Scripts/PogoAgogo/SectorController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class SectorController : MonoBehaviour
+{
+    public float repeatTime;
+    public AnimatorController animatorController;
+    public bool chooseSector;
+    public static int index; public static bool selectionActive;
+    public static List<Transform> sectors = new List<Transform>0);
+    private Color originalColor;
+
+    void Start()
+    {
+        selectionActive = true;
+        foreach (Transform child in transform)
+        {
+            sectors.Add(child.GetChild(0));
+        }
+
+        originalColor = sectors[0].GetComponent<MeshRenderer>().material.color;
+        StartCoroutine(WaitAndChange());
+    }
+
+    void Update()
+    {
+        if(chooseSector)
+        {
+            chooseSector = false;
+            selectionActive = false;
+            sectors[index].GetComponent<Animator>().runtimeAnimatorController = animatorController;
+            sectors[index].GetComponent<Animator>().SetTrigger("Move");
+        }
+    }
+
+    public static void RemoveAnimator()
+    {
+        selectionActive = true;
+    }
+
+    private void changeIndex()
+    {
+        index++;
+        if(index == sectors.Count)
+            index = 0 ;
+    }
+
+    private void SetColors()
+    {
+        for (int i = 0; i < sectors.Count; i++)
+        {
+            if (i == index)
+                sectors[i].GetComponent<MeshRenderer>().material.color = Color.red;
+            else
+                sectors[i].GetComponent<MeshRenderer>().material.color = originalColor;
+        }
+    }
+
+    private IEnumerator WaitAndChange()
+    {
+        while(true)
+        {
+            if(selectionActive)
+
+            {
+                changeIndex();
+                SetColors();
+            }       
+            yield return new WaitForSeconds(repeatTime);
+        }
+    }
+}
